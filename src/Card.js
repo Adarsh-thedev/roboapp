7912290dc276b224134be595f75f13215ff6aeeb
Adarsh-thedev <adarshpittu@gmail.com>
2020-01-24 00:25:43 +0530

    robots.js added, done with card component

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = (props) => {
+    const { name, email, id } = props;
+    return(
+        <React.Fragment>
+            <div className = 'bg-washed-red dib br3 ma2 pa3 grow bw2 shadow=5 tc'>
+                <img alt='robot' src = {`https://www.robohash.org/${id}?200x200`} />
+                <div>
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Card;
