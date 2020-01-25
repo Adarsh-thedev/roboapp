b3281c76836d44b4589be75191ee785b8eb322e2
Adarsh-thedev <adarshpittu@gmail.com>
2020-01-25 16:29:57 +0530

    calling robots from an api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import Searchbox from './Searchbox';
 import './app.css';
 
@@ -8,9 +7,15 @@ class App extends React.Component{
   constructor() {
     super();
     this.state = {
-      robots : robots,
+      robots : [],
       searchfield : ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({robots : users}))
   }
 
   onSearchChange = (event) =>{
@@ -22,13 +27,19 @@ class App extends React.Component{
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
     //console.log(filteredRobots);
-    return (
-      <div className='tc'>
-        <h1 className = 'f1 dim'>Robo World</h1>
-        <Searchbox searchChange = {this.onSearchChange}/>
-        <CardList robots = {filteredRobots} />
-      </div>
-    );
+    if(this.state.robots.length === 0 ) {
+      return(
+        <h1 className = 'f1 tc'>Loading ...</h1>
+      );
+    } else{
+        return (
+          <div className='tc'>
+            <h1 className = 'f1 dim'>Robo World</h1>
+            <Searchbox searchChange = {this.onSearchChange}/>
+            <CardList robots = {filteredRobots} />
+          </div>
+        );
+    }
   }
   
 }
